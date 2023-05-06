a6b15e1cc7f0bd48c12909402ac6a14a7c5b206c
sumeetwd25 <131378640+sumeetwd25@users.noreply.github.com>
2023-05-07 02:53:39 +0530

    Add files via upload

--- mysite v1.0/server.js
+++ mysite v1.0/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, () => {
     console.log("Listen on the port 3000...");
